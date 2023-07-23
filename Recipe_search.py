7731e5dbbc5718d4744363b51e5e19ebb356de77
Madzia-d <139500739+Madzia-d@users.noreply.github.com>
2023-07-23 17:48:57 +0100

    Update Recipe_search.py
    just added some changes we did together, and made an f-string to tidy up a little

--- Recipe_search.py
+++ Recipe_search.py
@@ -5,7 +5,7 @@ print('\nWelcome to our Recipe Finder!')
 
 
 # Recipe search function
-def recipe_search(ingredient, exclusions, cuisine_type, meal_type):
+def recipe_search(ingredient, ingredient2, exclusions, cuisine_type, meal_type):
     app_id = '7a360a89'
     app_key = 'd8cc8b979a47905b2f875163868ab2b1'
     parameters = {
@@ -13,13 +13,7 @@ def recipe_search(ingredient, exclusions, cuisine_type, meal_type):
         'mealType': meal_type,
     }
     try:
-        result = requests.get(
-            'https://api.edamam.com/search?q={}&app_id={}&app_key={}&excluded={}'.format(ingredient, app_id,
-                                                                                         app_key,
-                                                                                         exclusions,
-                                                                                         ),
-            params=parameters
-        )
+        result = requests.get(f'https://api.edamam.com/search?q={ingredient}&q={ingredient2}&app_id={app_id}&app_key={app_key}&excluded={exclusions}', params=parameters)
         result.raise_for_status()  # Raise an exception if the request was unsuccessful
 
         # Return the list of different recipes
@@ -33,14 +27,16 @@ def recipe_search(ingredient, exclusions, cuisine_type, meal_type):
 def userinput():
     # Prompt user for main ingredient
     ingredient = input('\nPlease enter an ingredient: ')
+    # prompt user for another ingredient
+    ingredient2 = input('\n Would you like to enter another ingredient?')
     # Prompt user for excluded ingredients
     exclusions = input('\nAny food allergies? ')
-    # Prompt user for calories
+    # Prompt user meal type
     meal_type = input('\nPlease enter a meal type, e.g Breakfast')
-    # Prompt user for meal type
+    # Prompt user for cuisine_type
     cuisine_type = input('\nWhat cuisine type would you like? e.g chinese etc: ')
     # Get results from API
-    results = recipe_search(ingredient, exclusions, cuisine_type, meal_type)
+    results = recipe_search(ingredient, ingredient2, exclusions, cuisine_type, meal_type)
 
     for result in results:
         recipe = result['recipe']
@@ -57,10 +53,10 @@ def userinput():
     write = input('Would you like to save the recipe to a file? Y/N: ').upper()
 
     if write == 'Y':
-        with open('my_recipe.txt', 'w+') as text_file:
+        with open('my_recipe.txt', 'a+') as text_file:
             label = recipe['label']
             website = recipe['url']
-            text_file.write(label + '\n' + website)
+            text_file.write(label + '\n' + website + '\n')
             print('Recipe successfully saved!')
     else:
         print('\nYou have selected "N", recipe not saved')
